5bbb8f84698fe07e9893fda729c7bf7e48f0ef07
mc0514 <mc0514@163.com>
2016-03-13 16:17:58 +0800

    add the emd file and the result

--- sEMG_Basic_Hand_movements_upatras/Database 1/drawSemg.py
+++ sEMG_Basic_Hand_movements_upatras/Database 1/drawSemg.py
@@ -1,5 +1,9 @@
 
 from numpy import *
+import matplotlib.pyplot as plt
+import emd_functions as emd
+
+
 
 def loadDataSet():
     dataMat=[];
@@ -41,6 +45,49 @@ def plotBestFit():
 
 #data=loadDataSet()
 #print data
-plotBestFit()
+#plotBestFit()
+y=loadDataSet()
+yArr=array(y)
+n=shape(yArr)[0]
+t=range(0,n)
+#print t
+xcord=[]
+for i in range(n):
+        xcord.append(yArr[i,1])
+#print xcord
+
+c,r=emd.saw_emd(t,xcord,bc='extrap')
+#print shape(c)
+#print c[1]
+mode1=[]
+mode2=[]
+mode3=[]
+mode4=[]
+mode5=[]
+mode6=[]
+mode7=[]
+for i in range(n):
+        mode1.append(c[i,0])
+        mode2.append(c[i,1])
+        mode3.append(c[i,2])
+        mode4.append(c[i,3])
+        mode5.append(c[i,4])
+        mode6.append(c[i,5])
+        mode7.append(c[i,6])
+#pf, = plt.plot(t, xcord)
+#pr, = plt.plot(t, r)
+#pcs = plt.plot(t, c, 'k-')
+#p1=plt.plot(t,mode1)
+#p2=plt.plot(t,mode2)
+#p3=plt.plot(t,mode3)
+#p4=plt.plot(t,mode4)
+#p5=plt.plot(t,mode5)
+#p6=plt.plot(t,mode6)
+p7=plt.plot(t,mode7)
+
+#plt.legend((p4,p5,p6),('IMF3','IMF4','IMF5'))
+
+#plt.legend((pf, pcs[0], pr), ('original function', 'modes', 'residual'))
+plt.show()
 
 
